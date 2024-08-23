e75057ecbec039ab528e8095527c2acc7bd5f5f0
Raj Singh <rajsinghast03@gmail.com>
2024-08-23 21:03:21 +0530

    changed relation name

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://neondb_owner:3tULYM2vKqVb@ep-ancient-lab-a1ip24ct.ap-southeast-1.aws.neon.tech/MockInterviewTool?sslmode=require',
-    }
-  };
+  schema: "./utils/schema.js",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:zjqC2ZrTV5kA@ep-purple-glade-a1gt6pu5.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+  },
+};
--- utils/schema.js
+++ utils/schema.js
@@ -1,6 +1,6 @@
 import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 
-export const MockInterview = pgTable("MockInterviewTool", {
+export const MockInterview = pgTable("mockInterview", {
   id: serial("id").primaryKey(),
   jsonMockResp: text("jsonMockResp").notNull(),
   jobPosition: varchar("jobPosition").notNull(),
@@ -8,18 +8,17 @@ export const MockInterview = pgTable("MockInterviewTool", {
   jobExperience: varchar("jobExperience").notNull(),
   createdBy: varchar("createdBy").notNull(),
   createdAt: varchar("createdAt"),
-  mockId: varchar("mockId").notNull()
+  mockId: varchar("mockId").notNull(),
 });
 
-
-export const UserAnswer=pgTable('useAnswer',{
-   id:serial('id').primaryKey(),
-   mockIdRef:varchar("mockId").notNull(),
-   question:varchar('question').notNull(),
-   correctAns:text('correctAns'),
-   userAns:text('userAns'),
-   feedback:text('feedback'),
-   rating:varchar('rating'),
-   userEmail:varchar('userEmail'),
-   createdAt:varchar('createdAt')
-})
+export const UserAnswer = pgTable("useAnswer", {
+  id: serial("id").primaryKey(),
+  mockIdRef: varchar("mockId").notNull(),
+  question: varchar("question").notNull(),
+  correctAns: text("correctAns"),
+  userAns: text("userAns"),
+  feedback: text("feedback"),
+  rating: varchar("rating"),
+  userEmail: varchar("userEmail"),
+  createdAt: varchar("createdAt"),
+});
